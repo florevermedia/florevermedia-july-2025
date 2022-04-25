4be2b758ce3c4d85dc32077bdf29acee9ee00f12
Doug <git@dougie.io>
2022-04-24 20:52:58 -0400

    yes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+ */
 //
 // Scripts
 //
@@ -50,5 +50,4 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
 });
